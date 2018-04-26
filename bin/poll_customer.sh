41c46f3a34655f14c534c3b2cb5c62013adcb392
kenthua <kenthua@google.com>
2018-04-26 19:57:27 -0400

    modified to pull load balancer external ip

--- bin/poll_customer.sh
+++ bin/poll_customer.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-URL="customer-tutorial.$(minishift ip).nip.io"
+#URL="customer-tutorial.$(minishift ip).nip.io"
+URL=`kubectl get svc -n tutorial | grep customer | awk '{print $4}'`
 
 while true
-do curl $URL
+do curl $URL:8080
 sleep .1
 done
 
